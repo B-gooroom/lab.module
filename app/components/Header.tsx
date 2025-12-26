8f62fc803be989007369851d1bb353fc5d6489ce
sean <j.sean1316@gmail.com>
2025-12-26 20:25:50 +0900

    FEAT[route]: 라우트 페이지 추가

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full">
-      {/* 헤더 컨텐츠 */}
-      <div className="flex items-center justify-between px-20 py-8 border-b border-gray-200">
+    <header className="w-full sticky top-0 z-50 bg-white/55 border-b border-white/25 bg-liquid">
+      {/* // <div className="absolute inset-0 rounded-[16px] backdrop-blur-[1px]"> */}
+      <div className="flex items-center justify-between px-20 py-8">
         {/* 로고 */}
         <Link href="/">
           <Image
@@ -23,20 +23,20 @@ export default function Header() {
           <ul className="flex gap-8 text-[16px] font-semibold">
             <li>
               <a
-                href="#about"
+                href="/about"
                 className="hover:text-blue-600 transition-colors"
               >
                 ABOUT
               </a>
             </li>
             <li>
-              <a href="#work" className="hover:text-blue-600 transition-colors">
+              <a href="/work" className="hover:text-blue-600 transition-colors">
                 WORK
               </a>
             </li>
             <li>
               <a
-                href="#contact"
+                href="/contact"
                 className="hover:text-blue-600 transition-colors"
               >
                 CONTACT
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "lab.MODULE",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="antialiased">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
